b541567f7334125b6db3f821dce8a726f09cbac0
Hugo Alberto Bedolla Reyes <habedollareyes@solvd.com>
2024-09-12 21:00:42 -0600

    OWDS-39: Set up common deployment pipeline for Kubernetes

--- src/bake.ts
+++ src/bake.ts
@@ -177,7 +177,7 @@ export class HelmRenderEngine extends RenderEngine {
          args.push('--set-json')
          for (const key in applicationSecretsAsJson) {
             if (key.startsWith("ENV_")) {
-               applicationSecrets[key] = Buffer.from(applicationSecretsAsJson[key], 'base64');
+               applicationSecrets[key] = Buffer.from(applicationSecretsAsJson[key], 'base64').toString();
             }
          }
          args.push(`applicationSecrets=${JSON.stringify(applicationSecrets)}`)
