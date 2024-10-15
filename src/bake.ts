14e1dec268cf72315fb8c466670235ffe5643a97
Hugo Alberto Bedolla Reyes <habedollareyes@solvd.com>
2024-10-14 22:23:18 -0600

    Enable pipeline for deploying to solvddev.com

--- src/bake.ts
+++ src/bake.ts
@@ -187,7 +187,7 @@ export class HelmRenderEngine extends RenderEngine {
          core.debug('Adding application secret inputs')
          args.push('--set-json')
          for (const key in applicationSecretsAsJson) {
-            if (key.startsWith("BENV_")) {
+            if (key.startsWith("RENV_")) {
                const secretKey = key.replace("RENV_", "")
                applicationSecrets[secretKey] = Buffer.from(applicationSecretsAsJson[key]).toString('base64');
             }
@@ -202,7 +202,7 @@ export class HelmRenderEngine extends RenderEngine {
          core.debug('Adding application variables inputs')
          args.push('--set-json')
          for (const key in applicationVariablesAsJson) {
-            if (key.startsWith("BENV_")) {
+            if (key.startsWith("RENV_")) {
                const secretKey = key.replace("RENV_", "")
                applicationVariables[secretKey] = applicationVariablesAsJson[key];
             }
